ca583904c370d8bfe6c3a1c51afdcdb5206f35f2
Steve Polyak <stevepolyak@mac.com>
2014-01-11 21:44:57 -0600

    change example to use mers module

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , student = require('./routes/student')
-  , http = require('http')
-  , path = require('path');
-
-//Load express-resource BEFORE app is instantiated
-var resource = require('express-resource');
+var express = require('express'), routes = require('./routes'), http = require('http'), path = require('path'), mers = require('mers');
 
 var app = express();
 
@@ -28,18 +19,63 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
-//mongo setup
-var mongoose = require('mongoose/');
+// mongo setup vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+var mongoose = require('mongoose/'), Schema = mongoose.Schema, ObjectId = mongoose.Schema.ObjectId;
 var mongourl;
+if (process.env.VCAP_SERVICES) {
+	// app is running in the cloud
+	var svcs = JSON.parse(process.env.VCAP_SERVICES);
+	mongourl = svcs['mongodb'][0].credentials.uri;
+} else {
+	// running locally or not on cloud foundry
+	mongourl = 'mongodb://localhost/act-rest-db';
+}
 
+var StudentAssessmentsSchema = new Schema({
+	title : String,
+	date : Date,
+	result : String
+});
+var StudentsSchema = new Schema({
+	firstName : String,
+	lastName : String,
+	birthDate : Date,
+	assessments : [ StudentAssessmentsSchema ]
+});
+StudentsSchema.statics.findNameLike = function findNameLike(q, term) {
+	return this.find({
+		 'firstName' : new RegExp(q.firstName || term, 'i')
+	});
+};
+var StudentAssessments = module.exports.StudentAssessments = mongoose.model(
+		'StudentAssessments', StudentAssessmentsSchema);
+var Students = module.exports.Students = mongoose.model('Students',
+		StudentsSchema);
+// mongo setup ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+app.use('/api/rest/v0.1', mers({
+	uri : mongourl,
+	error : function(err, req, res, next){
+		console.log(err);
+		if('CastError' === err.name && 'ObjectId' === err.type) {
+		  res.status(404).send({
+              status:404,
+              error:err && err.message
+          });
+		} else {
+		  res.status(500).send({
+	              status:500,
+	              error:err && err.message
+	      });	
+		}
+	}}).rest());
+
+          
 app.get('/', routes.index);
-app.get('/users', user.list);
 
-app.resource('students', require('./routes/student.js'));
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ACT REST API', req: req, apiContext: '/api/rest/v0.1/' });
 };
